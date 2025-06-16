6b67c321b123d35d41af0798ec83938a97f58d29
Lado <lnikolaishvili08@gmail.com>
2025-06-16 21:55:30 +0400

    home page styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,57 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/home";
-import AuthPages from "./pages/StarterPage";
-import Login from "./pages/login";
-import SignUp from "./pages/SignUp";
-import UserSelect from "./pages/UserSellect";
+import Home from "./pages/home.jsx";
+import AuthPages from "./pages/StarterPage.jsx";
+import Login from "./pages/login.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import UserSelect from "./pages/UserSellect.jsx";
+import Browse from "./pages/Browse.jsx";
+import Matches from "./pages/Matches.jsx";
+import Messages from "./pages/Messages.jsx";
+import { Settings } from "lucide-react";
+import Recommended from "./pages/Recommended.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/home",
     element: <Home />,
+    children: [
+      {
+        path: "browse",
+        element: <Browse />,
+      },
+      {
+        path: "matches",
+        element: <Matches />,
+      },
+      {
+        path: "messages",
+        element: <Messages />,
+      },
+      {
+        path: "recommended",
+        element: <Recommended />,
+      },
+      {
+        path: "settings",
+        element: <Settings />,
+      },
+    ],
   },
   {
-    path: "/", // changed from '*' to '/' for proper nested routing
+    path: "/",
     element: <AuthPages />,
     children: [
       {
-        path: "logIn", // removed leading slash
+        path: "logIn",
         element: <Login />,
       },
       {
-        path: "signUp", // removed leading slash
+        path: "signUp",
         element: <SignUp />,
       },
       {
-        path: "userSelect", // removed leading slash
+        path: "userSelect",
         element: <UserSelect />,
       },
     ],
